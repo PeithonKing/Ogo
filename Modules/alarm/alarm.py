8d1b58742bc8bb45314eca78fabe8f45782e8df2
PeithonKing <75820716+PeithonKing@users.noreply.github.com>
2021-05-17 21:01:01 +0530

    Update alarm.py

--- Modules/alarm/alarm.py
+++ Modules/alarm/alarm.py
@@ -7,17 +7,17 @@ alarmM = int(input("What minute do you want the alarm to ring? "))
 amPm = str(input("am or pm? "))
 
 
-if (amPm == "pm"):
+if amPm == "pm":
 		alarmH = alarmH + 12
-while(1 == 1):
-	os. system('cls')
+while True:
+	os.system('cls')
 	hour = datetime.datetime.now().hour
 	minute = datetime.datetime.now().minute
 	second = datetime.datetime.now().second
 	if (alarmM - minute - 1) >= 0:
 		print("Waiting for alarm: " + str(alarmH - hour) + " Hr " + str(alarmM - minute - 1) + " Min " + str(59 - second) + " sec ")
 		time.sleep(1)
-	os. system('cls')
+	os.system('cls')
 	if(alarmH == hour and alarmM == minute):
 		print("Time to wake up!")
 		playsound('beep 1.mpeg')
